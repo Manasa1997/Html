8b8578c8abb8cbe9d5a374691137a5e1700d779c
Manasa1997 <44022010+Manasa1997@users.noreply.github.com>
2018-12-03 17:51:43 +0530

    Add files via upload

---
+++ sub.js
@@ -0,0 +1,6 @@
+function doSub(){
+var  x=document.getElementById("fv").value;
+var y= document.getElementById("sv").value;
+var result=parseInt(x)-parseInt(y);
+document.write("sub is:"+result);
+}
