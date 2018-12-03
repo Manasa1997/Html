bfa004ae19abbc8c85f284256235951c013b721f
Manasa1997 <44022010+Manasa1997@users.noreply.github.com>
2018-12-03 17:47:08 +0530

    Add files via upload

---
+++ cal.js
@@ -0,0 +1,6 @@
+function doSum(){
+var  x=document.getElementById("fv").value;
+var y= document.getElementById("sv").value;
+var result=parseInt(x)+parseInt(y);
+document.write("sum is:"+result);
+}
