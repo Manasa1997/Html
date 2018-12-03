eb71c76100c66b7ef0644db38f863bbdf36a39e1
Manasa1997 <44022010+Manasa1997@users.noreply.github.com>
2018-12-03 17:52:08 +0530

    Add files via upload

---
+++ user1.js
@@ -0,0 +1,53 @@
+function validate(){
+var username=document.getElementById("user").value;
+var reg=/^[a-zA-Z0-9]{4,9}$/;
+if(username.match(reg)){
+document.getElementById('uerror').innerHTML="";
+}
+else{
+document.getElementById('uerror').innerHTML="<b style='color:red'>Invalid Username</b>";
+}
+var mail=document.getElementById("mail").value;
+var reg1=/^[a-zA-Z0-9]+[@][0-9A-Za-z]+[.][a-zA-Z]+$/;
+if(mail.match(reg1))
+{
+document.getElementById('merror').innerHTML="";
+}
+else{
+document.getElementById('merror').innerHTML="<b style='color:red'>Invalid Mail</b>";
+}
+var mobile=document.getElementById("Mobile").value;
+var reg2=/^[6-9][0-9]{9}$/;
+if(mobile.match(reg2))
+{
+document.getElementById('serror').innerHTML="";
+}
+else{
+document.getElementById('serror').innerHTML="<b style='color:red'>Invalid MObile Number</b>";
+}
+var pass=document.getElementById("pass").value;
+var reg3=/^(?=.*[0-9])(?=.*[a-z])(?=.*[_@#])(?=.*[a-z]).{8,}$/;
+if(pass.match(reg3))
+{
+document.getElementById('perror').innerHTML="";
+}
+else{
+document.getElementById('perror').innerHTML="<b style='color:red'>Invalid PassWord</b>";
+}
+var confirmpass=document.getElementById("cpass").value;
+if(pass=confirmpass)
+{document.getElementById('error').innerHTML="";
+}
+else{
+document.getElementById('error').innerHTML="<b style='color:red'>Invalid PassWord</b>";
+}
+var aadhar=document.getElementById("aadhar").value;
+var reg4=/^[0-9]{12,}$/
+if(aadhar.match(reg4)){
+document.getElementById('aerror').innerHTML="";
+}
+else{
+document.getElementById('aerror').innerHTML="<b style='color:red'>Invalid Aadhar Number</b>";
+}
+return false;
+}
